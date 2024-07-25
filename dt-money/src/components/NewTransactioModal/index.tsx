1c26341b42bdc2672679597d4d3ec8a8feb005bc
Mariana Dias <marianamkt6@gmail.com>
2024-07-25 17:06:01 -0300

    crianfo e estilizando os botõies de entrada e saida

--- dt-money/src/components/NewTransactioModal/index.tsx
+++ dt-money/src/components/NewTransactioModal/index.tsx
@@ -1,6 +1,7 @@
-import {X} from 'phosphor-react';
+import {ArrowCircleDown, ArrowCircleUp, X} from 'phosphor-react';
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+
 
 
 
@@ -19,6 +20,23 @@ export function NewTransactionModal(){
                     <input type="text" placeholder='Descrição' required />
                     <input type="number" placeholder='Preço' required />
                     <input type="text" placeholder='Categoria' required />
+
+                <TransactionType>
+                     <TransactionTypeButton variant='income'> 
+
+                        <ArrowCircleUp/>
+                            Entrada
+                         
+                    </TransactionTypeButton>
+
+                     <TransactionTypeButton variant='outcome'>
+
+                     <ArrowCircleDown/>
+                        Saída
+                        
+                    </TransactionTypeButton>
+                </TransactionType>
+
 
                     <button type="submit">Cadastrar</button>
                   </form>
