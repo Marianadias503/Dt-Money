90f1d59de1c5ad27f9cccc3bf5fa0e5de863d0e4
Mariana Dias <marianamkt6@gmail.com>
2024-07-25 15:15:01 -0300

    estilizando tabela de transações

--- dt-money/src/styles/transactions/index.tsx
+++ dt-money/src/styles/transactions/index.tsx
@@ -1,11 +1,44 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
     return (
         <div>
             <Header />
             <Summary/>
+
+           <TransactionsContainer>
+           <TransactionsTable>
+                <tbody>
+                    <tr>
+                        <td width="50%">Desenvolvimento do site</td>
+                        <td>
+                            <PriceHightLight variant="income">
+                            R$12.000,00
+                            </PriceHightLight>
+                         </td>
+                        <td>venda</td>
+                        <td>13/04/2022</td>
+                    </tr>
+                    <tr>
+                        <td width="50%">Hamburguer</td>
+                        <td>
+                            <PriceHightLight variant="outcome">
+                              - R$59,90
+                            </PriceHightLight>
+                         </td>
+                        <td>Alimentação</td>
+                        <td>10/04/2022</td>
+                    </tr>
+                    
+                </tbody>
+            </TransactionsTable>
+           </TransactionsContainer>
+
+
+
+
         </div>
     )
 }
